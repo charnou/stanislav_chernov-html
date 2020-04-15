c28e959fac070c04466a7807fcddda285f455055
stanislavchernovv <stanislavchernovv@gmail.com>
2020-04-15 10:38:35 +0300

    test fix

--- task6/test.js
+++ task6/test.js
@@ -1,39 +1,37 @@
 describe("sum", function () {
 
-    describe("складываются 2 положительных числа", function () {
-
-        it("Сумма 2 и 2 будет равна 4", function () {
-            assert.equal(sum(2, 2), 4);
-        });
-
-        it("Сумма 44 и 77 будет равна 121", function () {
-            assert.equal(sum(44, 77), 121);
-        });
-
+    it("Сумма 2-ух положительных чисел", function () {
+        assert.equal(sum(2, 2), 4);
+        assert.equal(sum(44, 77), 121);
+        assert.equal(sum(101, 909), 1010);
+        assert.equal(sum(2, 99999), 100001);
     });
 
-    describe("складываются 2 отрицательных числа", function () {
-
-        it("Сумма -111 и -222 будет равна -333", function () {
-            assert.equal(sum(-111, -222), -333);
-        });
-
+    it("Сумма 2-ух отрицательных чисел", function () {
+        assert.equal(sum(-1, -1), -2);
+        assert.equal(sum(-15, -42), -57);
+        assert.equal(sum(-209, -1), -210);
+        assert.equal(sum(-111, -222), -333);
+        assert.equal(sum(-2222, -8888), -11110);
     });
 
-    describe("складываются положительное и отрицательное число", function () {
-
-        it("Сумма 60 и -30 будет равна 30", function () {
-            assert.equal(sum(60, -30), 30);
-        });
-
+    it("Сумма положительных и отрицательных чисел", function () {
+        assert.equal(sum(2, -4), -2);
+        assert.equal(sum(60, -30), 30);
+        assert.equal(sum(515, -516), -1);
+        assert.equal(sum(10000, -9999), 1);
     });
 
-    describe("складываются число и пустая строка", function () {
+    it("Сумма чисел и пустых строк", function () {
+        assert.equal(sum('', 27), 27);
+        assert.equal(sum(219, ''), 219);
+        assert.equal(sum('', 100000000000), 100000000000);
+    });
 
-        it("Сумма пустой строки и 27 будет равна 27", function () {
-            assert.equal(sum('', 27), 27);
-        });
-
+    it("Сумма чисел в виде строк", function () {
+        assert.equal(sum('15', '15'), 30);
+        assert.equal(sum('-17000', '864939'), 847939);
+        assert.equal(sum('123456789', '987654321'), 1111111110);
     });
 
 });
