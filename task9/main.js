f0ce28ea0ab127162d958ebb5e8beb172aafbd40
Stanislav <stanislavchernovv@gmail.com>
2020-05-02 17:40:53 +0300

    refactor: changed fetch into xhr.
    fetch is still available in comments.

--- task9/main.js
+++ task9/main.js
@@ -1,70 +1,80 @@
 const staxios = {
 
-    get(url) {
-        return fetch(url)
-            .then(response => response.json())
-    },
+    sendRequest(method, url, body = null) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest;
 
-    post(url, object) {
-        return fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(object),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
+            xhr.open(method, url);
+
+            xhr.responseType = 'json';
+
+            xhr.setRequestHeader('Content-type', 'application/json')
+
+            xhr.onload = () => {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject(`${xhr.status} ${xhr.statusText}`);
                 }
-            })
-            .then(response => response.json())
-    },
+            }
 
-    put(url, object) {
-        return fetch(url, {
-                method: 'PUT',
-                body: JSON.stringify(object),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            })
-            .then(response => response.json())
-    },
+            xhr.onerror = () => {
+                reject(`${xhr.status} ${xhr.statusText}`);
+            };
 
-    delete(url) {
-        return fetch(url, {
-            method: 'DELETE'
+            if (method == 'DELETE') {
+                xhr.send();
+            } else {
+                xhr.send(JSON.stringify(body));
+            }
         })
     },
 
-    create(config) {
+    get(url) {
+        return this.sendRequest('GET', url);
+
+        // return fetch(url)
+        //     .then(response => response.json())
+    },
+
+    post(url, body) {
+        return this.sendRequest('POST', url, body);
+
+        // return fetch(url, {
+        //         method: 'POST',
+        //         body: JSON.stringify(body),
+        //     })
+        //     .then(response => response.json())
+    },
+
+    put(url, body) {
+        return this.sendRequest('PUT', url, body);
+
+        // return fetch(url, {
+        //         method: 'PUT',
+        //         body: JSON.stringify(object),
+        //         headers: {
+        //             "Content-type": "application/json; charset=UTF-8"
+        //         }
+        //     })
+        //     .then(response => response.json())
+    },
+
+    delete(url) {
+        return this.sendRequest('DELETE', url);
+
+        // return fetch(url, {
+        //     method: 'DELETE'
+        // })
+    },
+
+    create() {
         let instance = {};
-        instance.baseURL = config.baseURL;
-        instance.get = function () {
-            return fetch(this.baseURL)
-                .then(response => response.json())
-        }
-        instance.post = function (object) {
-            return fetch(this.baseURL, {
-                    method: 'POST',
-                    body: JSON.stringify(object),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                    }
-                })
-                .then(response => response.json())
-        }
-        instance.put = function (object) {
-            return fetch(this.baseURL, {
-                    method: 'PUT',
-                    body: JSON.stringify(object),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                    }
-                })
-                .then(response => response.json())
-        }
-        instance.delete = function () {
-            return fetch(this.baseURL, {
-                method: 'DELETE'
-            })
-        }
+        instance.sendRequest = this.sendRequest;
+        instance.get = this.get;
+        instance.post = this.post;
+        instance.put = this.put;
+        instance.delete = this.delete;
         return instance;
     }
 }
@@ -76,70 +86,56 @@ const object = {
     alive: true
 }
 
-staxios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(function (response) {
+staxios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
+    .then((response) => {
+        console.log(`GET`);
         console.log(response);
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch((error) => {
+        console.log(`GET`);
+        console.error(error);
     });
 
 staxios.post('https://jsonplaceholder.typicode.com/posts', object)
-    .then(function (response) {
+    .then((response) => {
+        console.log(`POST`);
         console.log(response);
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch((error) => {
+        console.log(`POST`);
+        console.error(error);
     });
 
-// staxios.put('https://jsonplaceholder.typicode.com/posts/1', object)
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
 
-// staxios.delete('https://jsonplaceholder.typicode.com/posts/1')
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-
-const instance = staxios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com/posts'
-})
-
-instance.get()
-    .then(function (response) {
+staxios.put('https://jsonplaceholder.typicode.com/posts/11', object)
+    .then((response) => {
+        console.log(`PUT`);
         console.log(response);
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch((error) => {
+        console.log(`PUT`);
+        console.error(error);
     });
 
-instance.post()
-    .then(function (response) {
+staxios.delete('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => {
+        console.log(`DELETE`);
+        console.log(response);
+        console.log('Delete success');
+    })
+    .catch((error) => {
+        console.log(`DELETE`);
+        console.error(error);
+    });
+
+const instance = staxios.create()
+
+instance.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => {
+        console.log(`instance GET`);
         console.log(response);
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch((error) => {
+        console.log(`instance GET`);
+        console.error(error);
     });
-
-// instance.put()
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-
-// instance.delete()
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
